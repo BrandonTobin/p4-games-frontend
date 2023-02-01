a408e43f02f5d4b9c058e26002c918c66d6cee8d
Brandon Tobin <brandontobinbusiness@gmail.com>
2023-02-01 08:44:04 -0600

    Added my pages/components and filled out my app.js there is no code in components/pages yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
+import AboutUs from "./pages/Aboutus";
+import UserDetails from "./components/UserDetails";
+import Games from "./pages/Games";
+import GameDetails from "./pages/GameDetails";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav">
+        <Link id="home" to="/">
+          <div> Home </div>
+        </Link>
+        <Link id="feed" to="/feed">
+          <div> Games </div>
+        </Link>
+        <Link id="authors" to="/authors">
+          <div> Users </div>
+        </Link>
+        <Link id="aboutus" to="/aboutus">
+          <div> About Us </div>
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetails />} />
+        <Route path="/games/:id" element={<GameDetails />} />
+        <Route path="/games/:id" element={<Reviews />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+      </Routes>
+    </>
   );
 }
 
